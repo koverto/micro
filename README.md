d279de9400efe78a1860d52bf8790eb38a8f686e
Jesse B. Hannah <jesse@jbhannah.net>
2020-02-21 10:27:11 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Koverto - Micro
+
+Wraps [go-micro][] as a base for gRPC server microservices.
+
+## Copyright
+
+Copyright © 2020 Jesse B. Hannah. Licensed under the [GNU GPL version 3 or
+later][gpl].
+
+[gpl]: LICENSE
+[go-micro]: https://github.com/micro/go-micro
---
+++ service.go
@@ -0,0 +1,40 @@
+package micro
+
+import (
+	"strings"
+
+	"github.com/micro/go-micro/v2"
+	"github.com/micro/go-micro/v2/config"
+	"github.com/micro/go-micro/v2/config/source"
+)
+
+// Service represents a microservice definition.
+type Service struct {
+	micro.Service
+	Name string
+	ID   string
+}
+
+// NewService initializes a new microservice instance with the given identifier
+// (e.g. com.example.svc.greeter), and given a configuration struct containing
+// default values, loads configuration values from the given sources into the
+// struct.
+func NewService(id string, conf interface{}, sources ...source.Source) (*Service, error) {
+	parts := strings.Split(id, ".")
+	name := parts[len(parts)-1]
+
+	service := micro.NewService(micro.Name(name))
+	service.Init()
+
+	if conf != nil {
+		if err := config.Load(sources...); err != nil {
+			return nil, err
+		}
+
+		if err := config.Scan(conf); err != nil {
+			return nil, err
+		}
+	}
+
+	return &Service{service, name, id}, nil
+}
